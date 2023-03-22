06bd77d1260a595f6b183a250b15d492e1b81944
gabrieldev6 <gabriel.christ.silva@gmail.com>
2023-03-22 17:52:21 -0300

    atualizando entidades

--- src/entities/Usuario.ts
+++ src/entities/Usuario.ts
@@ -28,15 +28,76 @@ export class Usuario {
   @JoinColumn({ name: "id_cargo" })
   id_cargo: Cargo;
 
+
   @OneToMany(() => Pedido, (pedido) => pedido.id_autorPedido)
-  AutorPedido: Pedido[];
+  autorPedido: Pedido[] | null;
 
   @OneToMany(() => Pedido, (pedido) => pedido.id_autorAutorizador)
-  AutorAutorizador: Pedido[];
+  autorAutorizador: Pedido[] | null;
 
   @OneToMany(
     () => PedidoAnonimo,
     (pedidoanonimo) => pedidoanonimo.id_autorAutorizadorAnonimo
   )
-  AutorAutorizadorAnonimo: PedidoAnonimo[];
+  autorAutorizadorAnonimo: PedidoAnonimo[] | null;
+
+  
+  constructor(nome: string, email: string, 
+              senha: string, id_cargo: Cargo, autorPedido: Pedido[] | null,
+              autorAutorizador: Pedido[] | null, autorAutorizadorAnonimo: PedidoAnonimo[]  | null) {
+
+    this.nome = nome
+    this.email = email
+    this.senha = senha
+    this.id_cargo = id_cargo
+    this.autorPedido = autorPedido
+    this.autorAutorizador = autorAutorizador
+    this.autorAutorizadorAnonimo = autorAutorizadorAnonimo
+    //this.validate()
+    
+  }
+  
+  
+  private validate() {
+    
+    const validatesenha = new RegExp("^(?=.*[A-Za-z])(?=.*?[0-9]).{6,}$")
+    const validateEmail = new RegExp("(([a-z]+\.?[a-z]+\.?[a-z]+[0-9]{2})+)(@aluno.ifce.edu.br)")
+
+    if(this.nome == "" || this.nome == null) {
+      throw new Error("nome é obrigatorio")
+    }
+    if(this.email == "" || this.email == null || !(validateEmail.test(this.email))) {
+      throw new Error("crie o email apartir do email institucional")
+    }
+    if(this.senha == "" || this.senha == null || !(validatesenha.test(this.senha))) {
+      throw new Error("senha é obrigatorio, devendo conter números, simbolos e letras maiusculas e minusculas ")
+    }
+    
+  }
+
+  public get Id_usuario() {
+    return this.id_usuario
+  }
+  public get Nome() {
+    return this.nome
+  }
+  public get Email() {
+    return this.email
+  }
+  public get Senha() {
+    return this.senha
+  }
+    
+  public get Id_cargo(){
+    return this.id_cargo
+  }
+  
+  public get AutorPedido() {
+    return this.autorPedido
+  }
+  
+  public get AutorAutorizador() {
+    return this.autorAutorizador
+  }
+  
 }
