1894d2a510dadf80f39a918395542678598f6e86
Luanffca <francaluan280@gmail.com>
2023-03-24 10:26:12 -0300

    Commit um

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import { DefaultTheme } from "./layouts/DefaultTheme";
 import { AllOrders } from "./pages/AllOrders";
 import { Home } from "./pages/Home";
-
+import { SignIn } from "./pages/SignIn";
+import {SignUp} from "./pages/SignUp";
 
 export function Router(){
     return(
         <Routes>
             <Route path="/" >
                 <Route path="/" element={<Home/>} />
+                <Route path="/signin" element={<SignIn/>} />
+                <Route path="/signup" element={<SignUp/>} />
             </Route>
 
             <Route path="/home" >
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -2,21 +2,6 @@
 import { GlobalStyle, Container, LoginContrainer, LoginWrap, LoginTitle, TextTitle, Input, ButtonDrpDwon, LoginButton} from './styles'
 
 export function SignUp(){
-    // const [nome, steNome] = useState('');
-    // const [Email, steEmail] = useState('');
-    // const [Matrícula, steMatrícula] = useState('');
-    // const [opcao, steOpcao] = useState('');
-    // const [Senha, steSenha] = useState('');
-
-    // const handleSubmit = (event) => {
-    //     event.prevenDefualt();
-    //     console.log("Formulário enviado!");
-    //     console.log("Nome:", nome);
-    //     console.log("E-mail:", Email);
-    //     console.log("Matrícula:", Matrícula);
-    //     console.log("Opcão:", opcao);
-    //     console.log("Senha:", Senha);
-    // };
     return (
         <>
         <GlobalStyle />
