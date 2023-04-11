5d1f981283638aa318ddbe81566555df5542764a
bryanlimac <bryan.contanto.cavalcante@gmail.com>
2023-04-10 22:20:21 -0300

    Correção do card de notificação

--- src/Components/RightNavBar/styles.ts
+++ src/Components/RightNavBar/styles.ts
@@ -2,18 +2,18 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     height: calc(100% - 6.75rem);
-    width: 353px;
+    width: 22.063rem;
     background-color: ${props => props.theme.colors["background-dark"]};
     position: absolute;
     right: 0;
     top: 6.75rem;
     @keyframes slideIn {
         from {
-            width: 0px;
+            width: 0rem;
         }
 
         to{
-            width: 353px;
+            width: 22.063rem;
         }
     }
 
