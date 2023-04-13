29115adfd75ef92221f6b1e2654f3cafbc6f2674
Melissa de Sousa <melissasousa6ms@gmail.com>
2023-04-13 14:32:39 -0300

    começo do login

--- backend/src/controllers/UsuarioController.ts
+++ backend/src/controllers/UsuarioController.ts
@@ -3,12 +3,17 @@ import { Response, Request } from "express";
 import { Usuario } from "../models/Usuario";
 import { Usuario as User } from "../entities/Usuario.entities";
 import * as bcrypt from "bcrypt";
+import { BadRequestError } from "../helpers/api-erros";
 export class UsuarioController {
   async create(req: Request, res: Response) {
     // criar usuário
     const { nome, email, senha, id_cargo } = req.body;
 
-    
+    const userExists = await usuarioRepository.findOneBy({ email });
+
+    if (userExists) {
+      throw new BadRequestError("Email já cadastrado ");
+    }
     let user = Usuario.create(nome, email, senha, id_cargo, null, null, null);
 
     const salt = bcrypt.genSaltSync(12);
