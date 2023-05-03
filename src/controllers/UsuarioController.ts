2e9c6404671484e777f01e3c070df96157879ebe
gabrieldev6 <gabriel.christ.silva@gmail.com>
2023-05-03 14:45:01 -0300

    atualização

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -3,6 +3,9 @@ import { Response, Request } from "express";
 import {Usuario } from "../models/Usuario"
 import { Usuario as User} from '../entities/Usuario.entities'
 import * as bcrypt from "bcrypt"
+import * as jwt from "jsonwebtoken"
+import { BadRequestError, UnauthorizedError} from "../helpers/api-erros"
+
 export class UsuarioController {
   async create(req: Request, res: Response) {
     // criar usuário
@@ -26,5 +29,37 @@ export class UsuarioController {
       console.log(error);
       return res.status(500).json({ message: "Internal Server Error" });
     }
+  }
+
+  async login (req: Request, res: Response) {
+    const { email, senha } = req.body;
+
+    const user = await usuarioRepository.findOneBy({ email });
+
+    if (!user) {
+      throw new BadRequestError("E-mail ou senha inválidos ");
+    }
+
+    const verifyPass = await bcrypt.compare(senha, user.senha);
+
+    if (!verifyPass) {
+      throw new BadRequestError("E-mail ou senha inválidos ");
+    }
+
+    //criando token para autenticar usuario
+    const token = jwt.sign(
+      { id: user.id_usuario },
+      process.env.JWT_PASS ?? "",
+      {
+        expiresIn: "8h",
+      }
+    );
+
+    const { senha: _, ...userLogin } = user;
+
+    return res.json({
+      user: userLogin,
+      token: token,
+    });
   }
 }
