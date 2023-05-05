57d3b34cb9d4607e38d68abb1fc01fab42376bb4
Melissa de Sousa <melissasousa6ms@gmail.com>
2023-05-05 18:12:30 -0300

    teste

--- backend/dist/controllers/UsuarioController.js
+++ backend/dist/controllers/UsuarioController.js
@@ -22,22 +22,71 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UsuarioController = void 0;
 const UsuarioRepository_1 = require("./../repositories/UsuarioRepository");
 const Usuario_1 = require("../models/Usuario");
 const bcrypt = __importStar(require("bcrypt"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const api_erros_1 = require("../helpers/api-erros");
 class UsuarioController {
     async create(req, res) {
         // criar usuário
         const { nome, email, senha, id_cargo } = req.body;
+        const userExists = await UsuarioRepository_1.usuarioRepository.findOneBy({ email });
+        if (userExists) {
+            throw new api_erros_1.BadRequestError("Email já cadastrado ");
+        }
         let user = Usuario_1.Usuario.create(nome, email, senha, id_cargo, null, null, null);
         const salt = bcrypt.genSaltSync(12);
         let senhaH = bcrypt.hashSync(senha, salt);
         user.senha = senhaH;
         const newUsuario = UsuarioRepository_1.usuarioRepository.create(user);
         await UsuarioRepository_1.usuarioRepository.save(newUsuario);
-        return res.status(201).json(newUsuario);
+        const { senha: _, ...userSemSenha } = newUsuario;
+        return res.status(201).json(userSemSenha);
+    }
+    async login(req, res) {
+        var _a;
+        const { email, senha } = req.body;
+        const user = await UsuarioRepository_1.usuarioRepository.findOneBy({ email });
+        if (!user) {
+            throw new api_erros_1.BadRequestError("E-mail ou senha inválidos ");
+        }
+        const verifyPass = await bcrypt.compare(senha, user.senha);
+        if (!verifyPass) {
+            throw new api_erros_1.BadRequestError("E-mail ou senha inválidos ");
+        }
+        //criando token para autenticar usuario
+        const token = jsonwebtoken_1.default.sign({ id: user.id_usuario }, (_a = process.env.JWT_PASS) !== null && _a !== void 0 ? _a : "", {
+            expiresIn: "8h",
+        });
+        const { senha: _, ...userLogin } = user;
+        return res.json({
+            user: userLogin,
+            token: token,
+        });
+    }
+    //usuario vai conseguir obter seus dados apartir do token
+    async getProfile(req, res) {
+        var _a;
+        const { authorization } = req.headers;
+        if (!authorization) {
+            throw new api_erros_1.UnauthorizedError("Não autorizado");
+        }
+        const token = authorization.split(" ")[1];
+        // verificando se o token existe
+        const { id_usuario } = jsonwebtoken_1.default.verify(token, (_a = process.env.JWT_PASS) !== null && _a !== void 0 ? _a : "");
+        const user = await UsuarioRepository_1.usuarioRepository.findOneBy({ id_usuario });
+        if (!user) {
+            throw new api_erros_1.UnauthorizedError("Não autorizado");
+        }
+        const { senha: _, ...loggedUser } = user;
+        return res.json(loggedUser);
+        console.log(token);
     }
 }
 exports.UsuarioController = UsuarioController;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import 'express-async-errors'
 import express from "express";
+import cors from "cors";
 import { AppDataSource } from "./data-source";
 import routes from "./routes";
 import { errorMiddleware } from './middlewares/error';
@@ -7,13 +8,20 @@ import { ApiError, BadRequestError, NotFoundError, UnauthorizedError } from "./h
 
 AppDataSource.initialize().then( async () => {
   const app = express();
-
+  const cors = require('cors');
   app.use(express.json());
 
   app.use(routes);
 
   app.use(errorMiddleware)
 
+  // Habilita CORS para todas as rotas
+  var corsOptions = {
+    origin: 'http://localhost/5432',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+  
   return app.listen(process.env.PORT);
 });
 
