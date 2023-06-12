2ae567968399fc0478e3dfb77e43fb358893ac80
Melissa de Sousa <melissasousa6ms@gmail.com>
2023-06-11 23:10:41 -0300

    implementação de novas rotas

--- backend/src/controllers/CargoController.ts
+++ backend/src/controllers/CargoController.ts
@@ -1,25 +1,127 @@
 import { cargoRepository } from "./../repositories/CargoRepository";
 import { Response, Request } from "express";
+import { BadRequestError } from "../helpers/api-erros";
+
 export class CargoController {
   async createCargo(req: Request, res: Response) {
-    // criar cargo
     const { cargo } = req.body;
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      throw new BadRequestError("Não autorizado");
+    }
 
     //validação básica
     if (!cargo) {
-      return res.status(400).json({message: "O cargo é obrigatório"});
+      return res.status(400).json({ message: "O cargo é obrigatório" });
     }
 
     try {
       const newCargo = cargoRepository.create({ cargo });
 
-      await cargoRepository.save(newCargo)
-      
-      return res.status(201).json(newCargo)
-      
+      await cargoRepository.save(newCargo);
+
+      return res.status(201).json(newCargo);
     } catch (error) {
       console.log(error);
       return res.status(500).json({ message: "Internal Server Error" });
     }
   }
+
+  async listCargos(req: Request, res: Response) {
+    try {
+      const cargos = await cargoRepository.find();
+  
+      return res.status(200).json(cargos);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+  
+  async getCargoById(req: Request, res: Response) {
+    const { id_cargo } = req.params;
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      throw new BadRequestError("Não autorizado");
+    }
+
+    try {
+      const cargo = await cargoRepository.findOne({
+        where: { id_cargo: parseInt(id_cargo) }, // Convertendo para número
+      });
+  
+      if (!cargo) {
+        return res.status(404).json({ message: "Cargo não encontrado" });
+      }
+  
+      return res.status(200).json(cargo);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+  
+  async updateCargo(req: Request, res: Response) {
+    const { id_cargo } = req.params;
+    const { cargo } = req.body;
+    const { authorization } = req.headers;
+  
+    if (!authorization) {
+      throw new BadRequestError("Não autorizado");
+    }
+  
+    // Validação básica
+    if (!cargo) {
+      return res.status(400).json({ message: "O cargo é obrigatório" });
+    }
+  
+    try {
+      const cargoToUpdate = await cargoRepository.findOne({
+        where: { id_cargo: parseInt(id_cargo) }, // Convertendo para número
+      });
+  
+      if (!cargoToUpdate) {
+        return res.status(404).json({ message: "Cargo não encontrado" });
+      }
+  
+      cargoToUpdate.cargo = cargo || cargoToUpdate.cargo;
+  
+      await cargoRepository.save(cargoToUpdate);
+  
+      return res.status(200).json(cargoToUpdate);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+  
+  async deleteCargo(req: Request, res: Response) {
+    const { id_cargo } = req.params;
+    const { authorization } = req.headers;
+  
+    if (!authorization) {
+      throw new BadRequestError("Não autorizado");
+    }
+  
+    try {
+      const cargo = await cargoRepository.findOne({
+        where: { id_cargo: parseInt(id_cargo) }, // Convertendo para número
+      });
+  
+  
+      if (!cargo) {
+        return res.status(404).json({ message: "Cargo não encontrado" });
+      }
+  
+      await cargoRepository.delete(id_cargo);
+  
+      return res.status(200).json({ message: "Cargo excluído com sucesso" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+  
 }
--- backend/src/controllers/HorarioController.ts
+++ backend/src/controllers/HorarioController.ts
@@ -1,14 +1,19 @@
 import { horarioRepository } from "./../repositories/HorarioRepository";
-
+import { BadRequestError } from "../helpers/api-erros";
 import { Request, Response } from "express";
-// import {}
+import jwt from "jsonwebtoken";
+
+type JwtPayload = {
+  id_usuario: number;
+};
+
 export class HorarioController {
   async createHorario(req: Request, res: Response) {
     const { dataInicio, dataFim, disponivel } = req.body;
 
     try {
       const newHorario = horarioRepository.create({
-        dataInicio,
+        dataInicio, // default = false
         dataFim,
         disponivel,
       });
@@ -33,22 +38,86 @@ export class HorarioController {
     }
   }
 
-//   async deleteHorario(req: Request, res: Response) {
-//     const { id_horaDisponivel } = req.params;
+  async getHorarioById(req: Request, res: Response) {
+    const { id_horario } = req.params;
+    const { authorization } = req.headers;
 
-//     try {
-//       const horario = await horarioRepository.findOne(id_horaDisponivel);
+    if (!authorization) {
+      throw new BadRequestError("Não autorizado");
+    }
 
-//       if (!horario) {
-//         return res.status(404).json({ message: 'Horário não encontrado' });
-//       }
+    try {
+      const horario = await horarioRepository.findOne({
+        where: { id_hora: parseInt(id_horario) }, // Convertendo para número
+      });
+  
+      if (!horario) {
+        return res.status(404).json({ message: 'Horário não encontrado' });
+      }
+  
+      return res.status(200).json(horario);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  }
+  
+  async updateHorario(req: Request, res: Response) {
+    const { id_horario } = req.params;
+    const { dataInicio, dataFim, disponivel } = req.body;
+    const { authorization } = req.headers;
 
-//       await horarioRepository.delete(id_horaDisponivel);
+    if (!authorization) {
+      throw new BadRequestError("Não autorizado");
+    }
 
-//       return res.status(200).json({ message: 'Horário excluído com sucesso' });
-//     } catch (error) {
-//       console.log(error);
-//       return res.status(500).json({ message: 'Internal Server Error' });
-//     }
-//   }
+    try {
+      
+
+      const horario = await horarioRepository.findOne({
+        where: { id_hora: parseInt(id_horario) }, // Convertendo para número
+      });
+
+      if (!horario) {
+        return res.status(404).json({ message: "Horário não encontrado" });
+      }
+
+      horario.dataInicio = dataInicio || horario.dataInicio;
+      horario.dataFim = dataFim || horario.dataFim;
+      horario.disponivel = disponivel || horario.disponivel;
+
+      await horarioRepository.save(horario);
+
+      return res.status(200).json(horario);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+
+    async deleteHorario(req: Request, res: Response) {
+      const { id_horario } = req.params;
+      const { authorization } = req.headers;
+
+      if (!authorization) {
+        throw new BadRequestError("Não autorizado");
+      }
+      try {
+        const horario = await horarioRepository.findOne({
+          where: { id_hora: parseInt(id_horario) }, // Convertendo para número
+        });
+  
+
+        if (!horario) {
+          return res.status(404).json({ message: 'Horário não encontrado' });
+        }
+
+        await horarioRepository.delete(id_horario);
+
+        return res.status(200).json({ message: 'Horário excluído com sucesso' });
+      } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+      }
+    }
 }
