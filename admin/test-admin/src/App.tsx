68c2c1145c74ff1911bc9e047bbd7f0c105e1a0e
gabrieldev6 <gabriel.christ.silva@gmail.com>
2023-06-18 16:36:27 -0300

    painel admin

--- admin/test-admin/src/App.tsx
+++ admin/test-admin/src/App.tsx
@@ -1,16 +1,18 @@
 
-import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
-import { dataProvider } from './dataProvider';
-import { authProvider } from './authProvider';
+import { Admin, ListGuesser, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server'
+import RestProvider from 'ra-data-simple-rest'
+import { UserList } from './userList/userList';
 
+const dataProvider = jsonServerProvider('http://localhost:3030')
 export const App = () => (
     <Admin
         dataProvider={dataProvider}
-		authProvider={authProvider}
 	>
-        <Resource name="root" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-		<Resource name="12345678" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="users" list={ListGuesser} />
+		
     </Admin>
+
 );
 
 //janedoe ; password
